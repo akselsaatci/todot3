62d2a1f019a8973dd3a1648e8dbd75b709a74e1f
akselsaatci <aksel.saatci1@gmail.com>
2023-01-22 13:00:28 +0300

    schema change

--- prisma/migrations/20230122082936_fix/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `toDo` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "toDo" DROP COLUMN "createdAt";
