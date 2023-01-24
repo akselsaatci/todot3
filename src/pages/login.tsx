456e14592b43d526230d6d27977ee0253c0efc65
akselsaatci <aksel.saatci1@gmail.com>
2023-01-25 00:02:23 +0300

    Login added. Logout functionality added. User can now login and logout. User can also see error messages when they try to login with invalid credentials.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,35 @@
 import { Helmet } from "react-helmet";
-import { signIn } from "next-auth/react";
+import { getSession, signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { GetServerSidePropsContext } from "next";
 
-const Login = () => {
-  async function clickHandle() {
-    await signIn();
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
 
+  return {
+    props: {ses: session },
+  };
+}
+
+
+
+const Login = () => {
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  async function clickHandle() {
+    await signIn('credentials', { redirect:true, callbackUrl: '/', username:username, password: password }).catch((err) => console.log(err));
+  }
+  const { error } = useRouter().query;
   return (
     <>
       <Helmet>
@@ -16,6 +40,7 @@ const Login = () => {
           <h1 className="pt-10 text-center text-5xl font-bold text-secondary">
             Login
           </h1>
+          {error && <SignInError error={error} />}
           <div className="flex justify-center pt-6">
             <form onSubmit={(e) => e.preventDefault()}>
               <div className="flex w-96 flex-col">
@@ -23,12 +48,16 @@ const Login = () => {
                   type="text"
                   placeholder="Username"
                   className="h-10 rounded-md border-2 border-thirdcolor bg-secondary pl-2"
+                  value={username}
+                  onChange={(e) => {setUsername(e.target.value)}}
                 />
                 <div className="pt-2"></div>
                 <input
-                  type="text"
+                  type="password"
                   placeholder="Password"
                   className="h-10 rounded-md border-2 border-thirdcolor bg-secondary pl-2"
+                  value={password}
+                  onChange={(e) => {setPassword(e.target.value)}}
                 />
                 <div className="pt-2"></div>
                 <div className="pt-2"></div>
@@ -37,7 +66,7 @@ const Login = () => {
                     void clickHandle().catch((err) => console.log(err));
                   }}
                   className="h-10 rounded-md bg-thirdcolor p-2 text-secondary"
-                ></button>
+                >Login</button>
               </div>
             </form>
           </div>
@@ -45,6 +74,25 @@ const Login = () => {
       </div>
     </>
   );
+};
+
+const errors = {
+  Signin: "Try signing with a different account.",
+  OAuthSignin: "Try signing with a different account.",
+  OAuthCallback: "Try signing with a different account.",
+  OAuthCreateAccount: "Try signing with a different account.",
+  EmailCreateAccount: "Try signing with a different account.",
+  Callback: "Try signing with a different account.",
+  OAuthAccountNotLinked:
+    "To confirm your identity, sign in with the same account you used originally.",
+  EmailSignin: "Check your email address.",
+  CredentialsSignin:
+    "Sign in failed. Check the details you provided are correct.",
+  default: "Unable to sign in.",
+};
+const SignInError:React.FC<{error:string|string[] }> = ({error}) => {
+  const errorMessage = error && (errors[error as keyof typeof errors] ?? errors.default);
+  return <div className="text-center mt-5 text-secondary">{errorMessage}</div>;
 };
 
 export default Login;
